a193d4f79d6b1fcba0044bbd59e0b8207b7139e4
ArjunSimform <arjunsingh.gurjar@simformsolutions.com>
2025-07-28 17:18:06 +0530

    remove : comments remove

--- src/utils/addImageToCloud.ts
+++ src/utils/addImageToCloud.ts
@@ -1,14 +1,10 @@
 const UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET;
-console.log('UPLOAD_PRESET', UPLOAD_PRESET);
-console.log('VITE_CLOUDINARY_URL', import.meta.env.VITE_CLOUDINARY_URL);
 const CLOUDINARY_URL = import.meta.env.VITE_CLOUDINARY_URL;
 
 export const uploadImage = async (file: File) => {
   const formData = new FormData();
   formData.append('file', file);
   formData.append('upload_preset', UPLOAD_PRESET);
-  console.log('formData', formData);
-  console.log('CLOUDINARY_URL', CLOUDINARY_URL);
 
   try {
     const res = await fetch(CLOUDINARY_URL, {
